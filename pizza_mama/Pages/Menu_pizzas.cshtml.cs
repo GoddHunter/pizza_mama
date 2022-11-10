37b8b6d3048ecb35bbcfcd7c3704f9afc72ecad6
Demillet GOKABA <demillet03@gmail.com>
2022-11-10 12:17:11 +0100

    ajout de la fenêtre MenuPizzas

--- pizza_mama/Pages/Menu_pizzas.cshtml.cs
+++ pizza_mama/Pages/Menu_pizzas.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using pizza_mama.Data;
+using pizza_mama.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace pizza_mama.Pages
 {
     public class Menu_pizzasModel : PageModel
     {
-        public void OnGet()
+        private readonly pizza_mama.Data.DataContext _context;
+
+        public Menu_pizzasModel(pizza_mama.Data.DataContext context)
         {
+            _context = context;
+        }
+        public IList<Pizza> Pizzas { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Pizzas = await _context.Pizzas.ToListAsync();
         }
     }
 }
